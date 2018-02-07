cbd60b92da72b2b3f4eac53c46ed22553d4873f9
Roman Vanesyan <romain.vanesyan@gmail.com>
2018-02-07 13:01:57 +0500

    add in-memory logging handler

--- lib/src/handlers/memory_handler.dart
+++ lib/src/handlers/memory_handler.dart
@@ -1,7 +1,16 @@
 import 'package:logger/logger.dart' show Record;
 import 'handler.dart';
 
+/// [MemoryHandler] implements in-memory logging [Record]s handler.
+/// It keeps sequence of logged records in call order, i.e. when [Record]
+/// is created it's simply appended to the list.
+/// The main scenario of [MemoryHandler] use - testing purposes.
 class MemoryHandler implements Handler {
+  /// Logged records.
+  final List<Record> records = <Record>[];
+
   @override
-  void call(Record record) {}
+  void call(Record record) {
+    records.add(record);
+  }
 }

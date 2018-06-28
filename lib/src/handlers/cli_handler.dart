3658a0aca9e2e94b8c5946b5430b55bc23357b51
Roman Vanesyan <romain.vanesyan@gmail.com>
2018-06-28 14:26:00 +0500

    Add minor tweaks to handlers

--- lib/src/handlers/cli_handler.dart
+++ lib/src/handlers/cli_handler.dart
@@ -1,12 +1,11 @@
-import 'package:logger/logger.dart' show Record;
-import 'package:logger/formatters.dart' show TextFormatter;
-import 'handler.dart';
+import 'package:logger/logger.dart' show Record, LogHandler;
+import 'package:logger/formatters.dart' show Formatter, TextFormatter;
 
-class CliHandler implements Handler {
-  final TextFormatter _formatter = new TextFormatter();
+class CliHandler implements LogHandler {
+  final Formatter formatter;
+
+  CliHandler({this.formatter = const TextFormatter()});
 
   @override
-  void call(Record record) {
-    print(_formatter.format(record));
-  }
+  void handleLog(Record record) => print(formatter.format(record));
 }
--- lib/src/handlers/memory_handler.dart
+++ lib/src/handlers/memory_handler.dart
@@ -1,16 +1,18 @@
-import 'package:logger/logger.dart' show Record;
-import 'handler.dart';
+import 'package:logger/logger.dart' show Record, LogHandler;
 
-/// [MemoryHandler] implements in-memory logging [Record]s handler.
+/// [MemoryHandler] implements in-memory logging handler.
+///
 /// It keeps sequence of logged records in call order, i.e. when [Record]
-/// is created it's simply appended to the list.
-/// The main scenario of [MemoryHandler] use - testing purposes.
-class MemoryHandler implements Handler {
+/// is created it's simply appended to the records list.
+///
+/// The main scenario of [MemoryHandler] use is testing.
+class MemoryHandler implements LogHandler {
   /// Logged records.
-  final List<Record> records = <Record>[];
+  final List<Record> _records = <Record>[];
+
+  /// A list of records fired by the logger on this handler.
+  List<Record> get records => List<Record>.unmodifiable(_records);
 
   @override
-  void call(Record record) {
-    records.add(record);
-  }
+  void handleLog(Record record) => _records.add(record);
 }

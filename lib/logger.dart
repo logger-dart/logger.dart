e687ee6363f15e68a1986429d26e87af1d5dab96
Roman Vanesyan <romain.vanesyan@gmail.com>
2018-06-28 14:24:31 +0500

    Change package structure   * Change to use `part`s to compose related files into the single library   * Exports only neccessary elements to the public in top level file (i.e. `logger.dart`)

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,4 +1,25 @@
-export 'src/level.dart';
-export 'src/logger.dart';
-export 'src/record.dart' show Record;
-export 'src/tracer.dart';
+/// Package provides simple, yet efficient structured logger.
+
+export 'src/package.dart'
+    show
+        Record,
+        Tracer,
+        LogHandler,
+        Interface,
+        Logger,
+        Level,
+
+        // default logger related things
+        defaultLogger,
+        level,
+        addHandler,
+        close,
+        log,
+        debug,
+        info,
+        warning,
+        error,
+        fatal,
+        trace,
+        withFields,
+        withField;
---
+++ lib/src/package.dart
@@ -0,0 +1,12 @@
+library logger;
+
+import 'dart:async' show Zone, Future, StreamController, Stream;
+
+part 'handler.dart';
+part 'level.dart';
+part 'tracer.dart';
+part 'record.dart';
+part 'interface.dart';
+part 'logger.dart';
+part 'context.dart';
+part 'default_logger.dart';

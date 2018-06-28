1e3f5760ee075c8e02bbf2f3b15fdcdc762fc4f4
Roman Vanesyan <romain.vanesyan@gmail.com>
2018-06-28 14:17:07 +0500

    Update CLI example to reflect a new logger creation API

--- example/cli_handler.dart
+++ example/cli_handler.dart
@@ -3,16 +3,15 @@ import 'package:logger/logger.dart';
 import 'package:logger/handlers.dart' show CliHandler;
 
 Future<void> main() async {
-  final logger = new Logger()..addHandler(new CliHandler());
+  final logger = Logger.createLogger('Example')..addHandler(CliHandler());
 
-  final context = logger.bind(<String, dynamic>{
+  final context = logger.withFields(<String, dynamic>{
     'username': 'vanesyan',
     'type': 'image/png',
     'image': 'avatar.png'
   });
 
-  context.info('uploading!');
-  final tracer = context.trace('uploaded!');
-  await new Future<void>.delayed(new Duration(milliseconds: 1000));
-  tracer.stop();
+  final tracer = context.trace('uploading!');
+  await Future<void>.delayed(const Duration(milliseconds: 1000));
+  tracer.stop('uploaded!');
 }

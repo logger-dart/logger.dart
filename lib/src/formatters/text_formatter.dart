9699bae3e510adc3fd3c3b4251922c9201898903
Roman Vanesyan <romain.vanesyan@gmail.com>
2018-06-28 14:47:46 +0500

    Minor tweaks with `TextFormatter` class

--- lib/src/formatters/text_formatter.dart
+++ lib/src/formatters/text_formatter.dart
@@ -2,39 +2,40 @@ import 'dart:collection';
 import 'package:logger/logger.dart' show Level, Record;
 import 'formatter.dart';
 
-class TextFormatter implements Formatter<String> {
-  TextFormatter();
+class TextFormatter implements Formatter {
+  final bool isColored;
+
+  const TextFormatter({this.isColored = true});
 
   @override
   String format(Record record) {
-    final buf = new StringBuffer();
+    final buf = StringBuffer();
 
+    _formatName(buf, record.name);
     _formatLevel(buf, record.level);
     _formatTime(buf, record.time);
     _formatMessage(buf, record.message);
     _formatFields(buf, record.fields);
-    _finalize(buf);
 
-    return buf.toString().trim();
+    return _finalize(buf);
   }
 
-  void _formatLevel(buf, Level level) => buf.write('[$level] ');
-
-  void _formatTime(buf, DateTime time) {
-    // buf.write();
-  }
+  void _formatName(StringBuffer buf, String name) => buf.write('$name ');
+  void _formatLevel(StringBuffer buf, Level level) => buf.write('$level ');
+  void _formatTime(StringBuffer buf, DateTime time) => buf.write('[$time] ');
 
   void _formatMessage(StringBuffer buf, String message) =>
-    buf.write(message.padRight(50));
+      buf.write(message.padRight(40));
 
   void _formatFields(StringBuffer buf, Map<String, dynamic> fields) {
-    Map<String, dynamic> _fields =
-        new SplayTreeMap<String, dynamic>.from(fields);
-
-    _fields.forEach((k, dynamic v) {
-      buf..write('$k=$v')..write(' ');
-    });
+    // Splay map is used here to alphabetically order fields;
+    // complexity: O(nlogn).
+    SplayTreeMap<String, dynamic>.from(fields)
+        .forEach((k, dynamic v) => buf..write(' $k=$v'));
   }
 
-  void _finalize(StringBuffer buf) => buf.writeln();
+  String _finalize(StringBuffer buf) {
+    buf.writeln();
+    return buf.toString().trim();
+  }
 }

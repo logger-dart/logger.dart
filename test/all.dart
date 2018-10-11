c43ab7c078b51629de7c51f750dad04e92f1cced
Roman Vanesyan <romain.vanesyan@gmail.com>
2018-10-12 00:20:22 +0500

    Add coveralls support

---
+++ test/all.dart
@@ -0,0 +1,9 @@
+import "discard_handler_test.dart" as discard_handler_test;
+import "level_test.dart" as level_test;
+import "logger_test.dart" as logger_test;
+
+void main() {
+  discard_handler_test.main();
+  level_test.main();
+  logger_test.main();
+}

61d1155b3d0d79c2162d50a8841710bfc9f80fe6
Roman Vanesyan <romain.vanesyan@gmail.com>
2018-10-30 17:54:59 +0500

    Add library annotation to default_logger

--- lib/default_logger.dart
+++ lib/default_logger.dart
@@ -1,1 +1,8 @@
+library logger.default_logger;
+
+/// The package shares a singleton logger used to quick start.
+///
+/// Use it only for quick tasks as it's always a good idea to create
+/// a new [Logger] instance rather than use the default logger.
+
 export "src/default_logger/default_logger.dart";

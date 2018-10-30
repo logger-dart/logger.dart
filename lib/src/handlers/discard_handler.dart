4d59bbb9cdeb4109d7695d0b3840a9d464995284
Roman Vanesyan <romain.vanesyan@gmail.com>
2018-10-30 17:55:46 +0500

    Cleanup DiscardHandler

--- lib/src/handlers/discard_handler.dart
+++ lib/src/handlers/discard_handler.dart
@@ -1,9 +1,7 @@
 import "package:logger/logger.dart" show Handler, Record;
 
-/// [DiscardHandler] is used to discard incoming logs.
+/// [DiscardHandler] is used to discard incoming records.
 class DiscardHandler extends Handler {
   @override
-  void call(Record record) {
-    // nobody
-  }
+  void call(Record record) {}
 }

59eeed4dc8af06ee8282aa2f60998ee4a71e4568
Roman Vanesyan <romain.vanesyan@gmail.com>
2018-10-30 20:41:35 +0500

    Update Formatter
    * Now Formatter is an abstract class
    * Formatter implements a stream transformer

--- lib/src/formatters/formatter.dart
+++ lib/src/formatters/formatter.dart
@@ -1,4 +1,38 @@
-import 'package:logger/logger.dart' show Record, LogHandler;
+import "dart:async"
+    show Stream, StreamController, StreamSubscription, StreamTransformerBase;
 
-/// Formatter is used to format incoming records in [LogHandler].
-typedef Formatter = dynamic Function(Record record);
+import "package:logger/logger.dart" show Record, Handler;
+
+/// Formatter is used to format log records and used by particular handler.
+abstract class Formatter extends StreamTransformerBase<Record, String> {
+  StreamController<String> _resultController;
+  Stream<Record> _recordStream;
+  StreamSubscription<Record> _recordSubscription;
+
+  /// Initializes formatter internals.
+  Formatter() {
+    _resultController = StreamController(onListen: _listen, onCancel: _cancel);
+  }
+
+  void _listen() {
+    _recordSubscription = _recordStream.listen((record) {
+      final result = call(record);
+
+      _resultController.add(result);
+    });
+  }
+
+  void _cancel() => _recordSubscription.cancel();
+
+  @override
+  Stream<String> bind(Stream<Record> stream) {
+    _recordStream = stream;
+
+    return _resultController.stream;
+  }
+
+  /// Formats the [record] to particular string representation.
+  ///
+  /// The [Formatter.call] may not modify the [record].
+  String call(Record record);
+}
